503ff832307e55fd35bd046675b45893d08cad89
ferbachega <ferbachega@gmail.com>
2015-10-30 19:23:26 -0200

    resolvido o problema da configuracao inicial para os atomos fixos "FIX"

--- GTKPyMOL.py
+++ GTKPyMOL.py
@@ -1676,6 +1676,14 @@ class gtkdynamo_main(threading.Thread,
                                'bg_color'           : 'black'  ,
                                'fixed'              : 'grey80' ,
                                'color'              : 'radon'  ,
+                               
+                               "FIX": {
+                                        "dots"   : False, 
+                                        "spheres": False, 
+                                        "lines"  : True, 
+                                        "sticks" : False
+                                      },
+
                                "QC": {
                                         "dots"   : False, 
                                         "spheres": True , 
